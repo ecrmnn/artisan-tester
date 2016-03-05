9705601a9fafbb8d430dde7acbaa830c1f1e6fd8
ecrmnn <danieleckermann@gmail.com>
2016-03-05 13:39:28 +0100

    init

---
+++ src/Providers/ArtisanTesterServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+namespace ArtisanTester\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class ArtisanTesterServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        // Boot
+    }
+
+    public function register()
+    {
+        // Register
+    }
+}
